a4dfd61ef8d3d105745fc55e729396ee8e94163c
Reed <reedjcutter@gmail.com>
2024-02-06 11:30:32 -0600

    Fixed connection when running in production env

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,15 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    }
   }
-
 };
